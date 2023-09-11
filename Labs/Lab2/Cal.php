720654be1ced95e71626a0836dceda52bbbfe98b
yazeedalma <143406917+yazeedalma@users.noreply.github.com>
2023-09-11 15:40:25 +0300

    Create Cal.php

---
+++ Labs/Lab2/Cal.php
@@ -0,0 +1,18 @@
+<?php
+function add($x,$y){
+return ($x+$y);
+}
+function subtract ($x,$y){
+return ($x-$y);
+}
+function multiply($x,$y){
+return ($x*$y);
+}
+function divide($x,$y){
+return ($x/$y);
+}
+echo"5+3=".add(5,3)".‹br>";
+echo"5-3=".subtract(5,3)".‹br>";
+echo"5*3=".multiply(5,3)".‹br>";
+echo"10/2=".divide(10,2)".‹br>";
+?>
